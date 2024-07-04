7bd0b8de19a93937b12035be3869bd9b44de58c1
moin-zaman <moinkhan758abzx@gmail.com>
2024-07-04 06:48:19 +0600

    Friend Class Part1 Updated

--- Class/ClassCoreConcept/FriendClass/main.cpp
+++ Class/ClassCoreConcept/FriendClass/main.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
+#include <cstring>
+#include <stdio.h>
 
 using namespace std;
 
 class A
 {
 private:
-    int id = 101;
-    string name = "Moin";
+    int id;
+    string name;
+
+public:
+    void set(int id, string name)
+    {
+        this -> id = id;
+        this -> name = name;
+    }
 
 public:
     friend class B;
 };
-
-
 
 class B
 {
@@ -23,12 +30,21 @@ public:
         cout << ob.id << endl;
         cout << ob.name << endl;
     }
+
 };
+
 
 int main()
 {
     A ob1;
     B ob2;
 
+
+//    ob2.display(ob1);
+
+
+    ob1.set(101,"Moin Zaman");
+
     ob2.display(ob1);
+
 }
