dcd2e5f45c2abf43be79bad081d2f395f06ef9e2
moin-zaman <moinkhan758abzx@gmail.com>
2024-07-11 04:00:48 +0600

    Updated Student Array

--- Array/StudentApplicArr_2/main.cpp
+++ Array/StudentApplicArr_2/main.cpp
@@ -1,65 +1,50 @@
 #include <iostream>
+#include <cstring>
+
 
 using namespace std;
 
 int main()
 {
+
     int n;
-    cout << "Enter the Size of an student arr : ";
+    cout << "Enter the Size of an Student  array : ";
     cin >> n;
 
     int stdArr[n];
 
+
     for(int i = 0; i < n; i++)
     {
-        cout << "Enter the marks of std [" << i+1 << "] : ";
+        cout << "stdArr[" << i << "] = ";
         cin >> stdArr[i];
     }
 
-    cout << "Marks of Student : ";
+    cout << "So Marks of Student" << endl;
+
+    cout << "stdArr[" << n << "] = {";
 
     for(int i = 0; i < n; i++)
     {
         cout << stdArr[i] << " ";
     }
-    cout << endl << endl;
+    cout << "}" << endl;
 
     int total = 0;
 
     for(int i = 0; i < n; i++)
     {
-        total = total+stdArr[i];
+        total = total + stdArr[i];
     }
 
-    cout << "Total marks is : " << total << endl;
-    double avg = (double)total/n;
-    cout << "Average is : " << avg << endl << endl;
+    cout << "Total Marks of Student = " << total << endl;
+
+    double avg = (double) total / n;
+
+    cout << "Average Marks of Student = " << avg << endl;
 
     int max = stdArr[0];
     int min = stdArr[0];
-
-//    for(int i = 1; i < n; i++)
-//    {
-//        if(max < stdArr[i])
-//        {
-//            max = stdArr[i];
-//        }
-//    }
-//
-//    cout << "Maximum marks is : " << max << endl;
-//
-//
-//    for(int i = 1; i < n; i++)
-//    {
-//        if(min > stdArr[i])
-//        {
-//            min = stdArr[i];
-//        }
-//    }
-//
-//    cout << "Minimum marks is : " << min << endl;
-
-
 
     for(int i = 1; i < n; i++)
     {
@@ -73,8 +58,33 @@ int main()
         }
     }
 
-    cout << "Maximum marks is : " << max << endl;
-    cout << "Minimum marks is : " << min << endl;
+    cout << "Maximum Mark is " << max << endl;
+    cout << "Minimum Mark is " << min << endl;
+
+    int findValue;
+    cout << "Please enter the value you want to search into the array : ";
+    cin >> findValue;
+
+    int count = -1;
+
+    for(int i = 0; i < n; i++)
+    {
+        if(findValue == stdArr[i])
+        {
+            count = i;
+        }
+    }
+
+    if(count == -1)
+    {
+        cout << findValue << " is not available into the array" << endl;
+
+    }
+    else
+    {
+        cout << "The index of " << findValue << " is = " << count << endl;
+    }
+
 
 
 }
